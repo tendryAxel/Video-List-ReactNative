aa80188ef2a700abccc15e7be2425a3aa3c16478
tendryAxel <hei.tendry@gmail.com>
2025-03-09 18:56:23 +0300

    chore: all CRUD operation for video table

--- src/services/videosService.ts
+++ src/services/videosService.ts
@@ -1,6 +1,7 @@
 import { VideoType } from "@/components/video/type"
 import { videosTable } from "../../db/schema"
 import { db } from "../../db/repository"
+import { eq } from "drizzle-orm"
 
 function mapImageRepresentationUrlNullToUndefined<T>(value: {
   imageRepresentationUrl: string | null
@@ -17,3 +18,31 @@ export const getAll = (): VideoType[] =>
     .from(videosTable)
     .all()
     .map(mapImageRepresentationUrlNullToUndefined<VideoType>)
+
+const save = (videos: (typeof videosTable.$inferInsert)[]): VideoType[] => {
+  return db
+    .insert(videosTable)
+    .values(videos)
+    .returning()
+    .all()
+    .map(mapImageRepresentationUrlNullToUndefined<VideoType>)
+}
+const update = (video: typeof videosTable.$inferInsert): VideoType | null => {
+  if (!video.id) return null
+  return mapImageRepresentationUrlNullToUndefined(
+    db.update(videosTable).set(video).where(eq(videosTable.id, video.id)).returning().get(),
+  )
+}
+export const getById = (videoId: number): VideoType | null => {
+  const result = db.select().from(videosTable).where(eq(videosTable.id, videoId)).get()
+  return result ? mapImageRepresentationUrlNullToUndefined(result) : null
+}
+export const deleteById = (videoId: number): VideoType | null => {
+  const result = db.delete(videosTable).where(eq(videosTable.id, videoId)).returning().get()
+  return result ? mapImageRepresentationUrlNullToUndefined(result) : null
+}
+export const crupdate = (videos: VideoType[]): VideoType[] => {
+  const updated = videos.map(update).filter((video) => video !== null)
+  const updatedId = updated.map((video) => video.id)
+  return [...updated, ...save(videos.filter((video) => !(video.id in updatedId)))]
+}
